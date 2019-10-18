908d05c58115b03cac00c376815ebc9dc913830f
Ubpa <641614112@qq.com>
2019-10-19 01:22:11 +0800

    [2019Autumn][CGAD] hw5 v1.1

--- 2019Autumn/CAGD/homeworks/05/codes/DeBoorPoints.m
+++ 2019Autumn/CAGD/homeworks/05/codes/DeBoorPoints.m
@@ -11,32 +11,46 @@ function d = DeBoorPoints(k, s)
     k = [k(1,:);k;k(size(k,1), :)];
     
     A = zeros(n+3);
-    b = k;
+    b = k(:,1:2);
     
     for i=1:(n+3)
         idx = i+2; % for t
         if t(idx) == t(idx-1)
-            if t(idx) == t(idx+1)
-                % end point
+            if t(idx) == t(idx+1) % 尖点
                 A(i, i) = 1;
-            else
-                % left natural
+            elseif k(i,3) == 1 % 直线首
+                A(i,i) = 1;
+            elseif k(i-1,3) == 1 % 直线右自然
+                A(i,i-2) = t(idx+1) - t(idx-1);
+                A(i,i-1) = -(t(idx+1)-t(idx-1)) - (t(idx)-t(idx-2));
+                A(i,i) = t(idx) - t(idx-2);
+                b(i,:) = [0, 0];
+            else %尖点右自然
                 A(i,i-1) = t(idx+2) - t(idx);
                 A(i,i) = -(t(idx+2)-t(idx)) - (t(idx+1)-t(idx-1));
                 A(i,i+1) = t(idx+1) - t(idx-1);
                 b(i,:) = [0, 0];
             end
         elseif t(idx) == t(idx+1)
-            % right natural
-            A(i,i-1) = t(idx+1) - t(idx-1);
-            A(i,i) = -(t(idx+1)-t(idx-1)) - (t(idx)-t(idx-2));
-            A(i,i+1) = t(idx) - t(idx-2);
-            b(i,:) = [0, 0];
+            if k(i,3) == 1 % 直线尾
+                A(i,i) = 1;
+            elseif k(i+1,3) == 1 % 下一直线首
+                % 直线左自然
+                A(i,i) = t(idx+2) - t(idx);
+                A(i,i+1) = -(t(idx+2)-t(idx)) - (t(idx+1)-t(idx-1));
+                A(i,i+2) = t(idx+1) - t(idx-1);
+                b(i,:) = [0, 0];
+            else
+                %尖点左自然
+                A(i,i-1) = t(idx+1) - t(idx-1);
+                A(i,i) = -(t(idx+1)-t(idx-1)) - (t(idx)-t(idx-2));
+                A(i,i+1) = t(idx) - t(idx-2);
+                b(i,:) = [0, 0];
+            end
         else
             A(i,i-1) = N(t,idx-3,4,s(idx-3));
             A(i,i) = N(t,idx-2,4,s(idx-3));
             A(i,i+1) = N(t,idx-1,4,s(idx-3));
-            b(i,:) = k(i,:);
         end
     end
     
--- 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
+++ 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
@@ -3,16 +3,42 @@ function DrawBSpline(p, isDrawCtrl)
 nP = size(p,1);
 
 % expand
-k = zeros(sum(p(:,3)), 2);
-s = zeros(sum(p(:,3)), 1);
-curIdx = 1;
-for i = 1 : nP
-    m = p(i,3); % multiplicity
-    k(curIdx : (curIdx+m-1), :) = repmat(p(i,1:2),[m,1]);
-    s(curIdx : (curIdx+m-1)) = repmat(i-1,[m,1]);
-    curIdx = curIdx + m;
+k = zeros(0,3);%第三维用来标识直线的首或尾
+s = zeros(0,1);
+i = 1;
+while i <= nP
+    if p(i,3) == 2 % 直线
+        k = [k;p(i,1:2), 0];
+        k = [k;p(i,1:2), 1]; % 直线首
+        s = [s;i-1];
+        s = [s;i-1];
+        while i+2 <= nP && p(i+2, 3) == 2 % 直线
+            i = i+1;
+            k = [k; p(i,1:2), 0];
+            k = [k; p(i,1:2), 0];
+            k = [k; p(i,1:2), 0];
+            s = [s;i-1];
+            s = [s;i-1];
+            s = [s;i-1];
+        end
+        i = i+1;
+        k = [k; p(i,1:2), 1]; % 直线尾
+        k = [k; p(i,1:2), 0];
+        s = [s; i-1];
+        s = [s; i-1];
+    elseif p(i,3) == 3 % 尖点
+        k = [k; repmat([p(i,1:2),0], [3,1])];
+        s = [s;i-1];
+        s = [s;i-1];
+        s = [s;i-1];
+    else % C2
+        k = [k; p(i,1:2), 0];
+        s = [s;i-1];
+    end
+    i = i +1;
 end
-
+k
+s
 d = DeBoorPoints(k, s);
 
 if size(d,1)>2
