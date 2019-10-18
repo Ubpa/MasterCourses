da6e65f3e6dd9e32359c513f236aa6b542f33f95
Ubpa <641614112@qq.com>
2019-10-19 02:59:00 +0800

    [2019Autumn][CGAD] hw5 v2.0

--- 2019Autumn/CAGD/homeworks/05/codes/DeBoorPoints.m
+++ 2019Autumn/CAGD/homeworks/05/codes/DeBoorPoints.m
@@ -53,41 +53,6 @@ function d = DeBoorPoints(k, s)
             A(i,i+1) = N(t,idx-1,4,s(idx-3));
         end
     end
-    
-    % natural end conditions
-%     alpha(1) = s(3) - s(1);
-%     beta(1) = -(s(3)-s(1))-(s(2)-s(1));
-%     gamma(1) = s(2) - s(1);
-%     
-%     alpha(n+1) = s(n+1) - s(n);
-%     beta(n+1) = -(s(n+1)-s(n)) - (s(n+1)-s(n-1));
-%     gamma(n+1) = s(n+1) - s(n-1);
-%     
-%     for i = 2:n
-%         alpha(i) = N(t, i, 4, s(i));
-%         beta(i) = N(t, i+1, 4, s(i));
-%         gamma(i) = N(t, i+2, 4, s(i));
-%     end
-%     
-%     % TODO Thomas Ëã·¨
-%     A = zeros(n+3);
-%     A(1,1)=1;
-%     A(n+3, n+3)=1;
-%     for i = 2:(n+2)
-%         A(i, i-1) = alpha(i-1);
-%         A(i, i) = beta(i-1);
-%         A(i, i+1)=gamma(i-1);
-%     end
-%     
-%     b = zeros(n+3, 2);
-%     b(1,:) = k(1,:);
-%     b(n+3,:) = k(n+1,:);
-%     
-%     for i = 3:(n+1)
-%         b(i, :) = k(i-1,:);
-%     end
-    
-    [A,b]
-    
+    A
     d = A \ b;
 end
--- 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
+++ 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
@@ -7,40 +7,31 @@ k = zeros(0,3);%第三维用来标识直线的首或尾
 s = zeros(0,1);
 i = 1;
 while i <= nP
-    if p(i,3) == 2 % 直线
+    if p(i,3) == 2 && i ~= nP && p(i+1,3) == 2% 直线
         k = [k;p(i,1:2), 0];
         k = [k;p(i,1:2), 1]; % 直线首
-        s = [s;i-1];
-        s = [s;i-1];
+        s = [s; repmat(i-1, [2,1])];
         while i+2 <= nP && p(i+2, 3) == 2 % 直线
             i = i+1;
-            k = [k; p(i,1:2), 0];
-            k = [k; p(i,1:2), 0];
-            k = [k; p(i,1:2), 0];
-            s = [s;i-1];
-            s = [s;i-1];
-            s = [s;i-1];
+            k = [k; repmat([p(i,1:2),0], [3,1])];
+            s = [s; repmat(i-1, [3,1])];
         end
         i = i+1;
         k = [k; p(i,1:2), 1]; % 直线尾
         k = [k; p(i,1:2), 0];
-        s = [s; i-1];
-        s = [s; i-1];
+        s = [s; repmat(i-1, [2,1])];
     elseif p(i,3) == 3 % 尖点
         k = [k; repmat([p(i,1:2),0], [3,1])];
-        s = [s;i-1];
-        s = [s;i-1];
-        s = [s;i-1];
+        s = [s; repmat(i-1, [3,1])];
     else % C2
         k = [k; p(i,1:2), 0];
         s = [s;i-1];
     end
     i = i +1;
 end
-k
-s
 d = DeBoorPoints(k, s);
 
+% draw B-Spline
 if size(d,1)>2
     sX = (s(1):0.01:s(size(s,1)))';
     sN = size(sX, 1);
@@ -49,7 +40,7 @@ if size(d,1)>2
         sY(i,:) = BSpline(s, d, sX(i));
     end
 
-    plot(sY(:,1), sY(:,2));
+    plot(sY(:,1), sY(:,2), 'LineWidth', 3);
 end
 
 % draw ctrl
@@ -59,8 +50,9 @@ if isDrawCtrl
         plot(p(i,1), p(i,2), config(p(i,3),:), 'MarkerSize', 32);
     end
     
-    plot(d(:,1),d(:,2),'c');
+    plot(d(:,1),d(:,2),'c--');
+    
      for i = 1:size(d,1)
-         plot(d(i,1),d(i,2), 'c.', 'MarkerSize', 32);
+         plot(d(i,1),d(i,2), 'c.', 'MarkerSize', 16);
      end
 end
