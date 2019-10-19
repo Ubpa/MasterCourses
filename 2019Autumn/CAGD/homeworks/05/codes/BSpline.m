a964d169ccb9783ceaa22f1b16167e62cc1e26e6
Ubpa <641614112@qq.com>
2019-10-19 12:43:17 +0800

    [2019Autumn][CGAD] hw5 v2.4
    fast plot

--- 2019Autumn/CAGD/homeworks/05/codes/BSpline.m
+++ 2019Autumn/CAGD/homeworks/05/codes/BSpline.m
@@ -3,7 +3,8 @@ function rst = BSpline(s, d, x)
         rst = d(size(d,1), :);
         return;
     end
-
+    
+    % binary serach
     left = 1; % s(left) <= x
     right = size(s,1); % s(right) > x
     while right - left > 1
@@ -19,15 +20,36 @@ function rst = BSpline(s, d, x)
     
     % extent t
     t = [s(1); s(1); s(1); s; s(size(s,1)); s(size(s,1)); s(size(s,1))];
-    i = left;
-%     if i == size(s,1)
-%         rst = d(size(d,1),:);
-%         return;
-%     end
     
-    c1 = N(t, i, 4, x);
-    c2 = N(t, i+1, 4, x);
-    c3 = N(t, i+2, 4, x);
-    c4 = N(t, i+3, 4, x);
-    rst = c1 * d(i, :) + c2 * d(i+1,:) + c3 * d(i+2, :) + c4 * d(i+3, :);
+%     idx = left;
+%     c1 = N(t, idx, 4, x);
+%     c2 = N(t, idx+1, 4, x);
+%     c3 = N(t, idx+2, 4, x);
+%     c4 = N(t, idx+3, 4, x);
+%     rst = c1 * d(idx, :) + c2 * d(idx+1,:) + c3 * d(idx+2, :) + c4 * d(idx+3, :);
+    
+    curN = zeros(7,1);
+    for i = 1:7
+    	curN(i) = N(t,left+i-1,1,x);
+    end
+    for i =2:4 % k
+        for j = 1:8-i
+            idx = left + j -1;
+            if t(idx+i-1) - t(idx) == 0
+                l = 0;
+            else
+                l = (x-t(idx))/(t(idx+i-1) - t(idx)) * curN(j);
+            end
+
+            if t(idx + i) - t(idx + 1) == 0
+                r = 0;
+            else
+                r = (t(idx + i) - x)/(t(idx + i) - t(idx+1)) * curN(j+1);
+            end
+
+            curN(j) = l + r;
+        end
+    end
+    rst = curN(1) * d(left, :) + curN(2) * d(left+1,:) + curN(3) * d(left+2, :) + curN(4) * d(left+3, :);
+    
 end
--- 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
+++ 2019Autumn/CAGD/homeworks/05/codes/DrawBSpline.m
@@ -13,7 +13,7 @@ while i <= nP
         k(idx+1,:) = [p(i,1:2), 1]; % 直线首
         s(idx:idx+1) = repmat(i-1, [2,1]);
         idx = idx + 2;
-        while i+2 <= nP && p(i+2, 3) == 2 % 直线
+        while i+2 <= nP && p(i+2, 3) == 2 % 直线中部
             i = i+1;
             k(idx:idx+2,:) = repmat([p(i,1:2),0], [3,1]);
             s(idx:idx+2) = repmat(i-1, [3,1]);
@@ -48,20 +48,22 @@ if size(d,1)>2
     for i = 1: sN
         sY(i,:) = BSpline(s, d, sX(i));
     end
-
+    
+tic;
     plot(sY(:,1), sY(:,2), 'LineWidth', 3);
+toc;
 end
 
 % draw ctrl
 if isDrawCtrl
-    config = ['r.';'g.';'b.'];
-    for i = 1:nP
-        plot(p(i,1), p(i,2), config(p(i,3),:), 'MarkerSize', 32);
-    end
+    rR = p(:,3)==1;
+    gR = p(:,3)==2;
+    bR = p(:,3)==3;
+    plot(p(rR,1), p(rR,2), 'r.', 'MarkerSize', 32);
+    plot(p(gR,1), p(gR,2), 'g.', 'MarkerSize', 32);
+    plot(p(bR,1), p(bR,2), 'b.', 'MarkerSize', 32);
     
     plot(d(:,1),d(:,2),'c--');
     
-     for i = 1:size(d,1)
-         plot(d(i,1),d(i,2), 'c.', 'MarkerSize', 16);
-     end
+    plot(d(:,1),d(:,2), 'c.', 'MarkerSize', 16);
 end
